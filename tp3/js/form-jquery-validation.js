4b0007bd1b25142d239e87f95fe7f6c86def9327
sabrine belmekki <32839750+sabrinebelmekki@users.noreply.github.com>
2017-11-15 17:35:45 +0100

    Update form-jquery-validation.js

--- tp3/js/form-jquery-validation.js
+++ tp3/js/form-jquery-validation.js
@@ -14,7 +14,9 @@ $(document).ready(function () { //pour chargement de la page
                 $('#myModal').modal("show");
             }
             else {
-                $(".modal-body").html(' <img style="max-width:100%" src="https://maps.googleapis.com/maps/api/staticmap?center='+$('#inputAdresse').val()+'&markers='+$('#inputAdresse').val()+'&size=800x400&zoom=13&key=AIzaSyC-3seQLhWm4wNaI6JeXOdHNLfIWknbN88"/>');
+                $(".modal-body").html (' Votre date de naissance est : ' + $("#inputdatedenaissance").val() + 
+            '</br>'  + 'Et vous habitez : ' + '</br>'  + 
+            ' <img style="max-width:100%" src="https://maps.googleapis.com/maps/api/staticmap?center='+$('#inputAdresse').val()+'&markers='+$('#inputAdresse').val()+'&size=800x400&zoom=13&key=AIzaSyC-3seQLhWm4wNaI6JeXOdHNLfIWknbN88"/>');
                 $(".modal-title").text("Bienvenue "+$("#InputNom").val());
                 $('#myModal').modal("show");
     
